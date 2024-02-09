e1b0a2a75154a4376c962ed86c9bd41f6250da33
Duranx <francis.duranx@gmail.com>
2024-02-09 15:41:42 +0100

    ajout de la fonction save_heuristique_statique pour faire la difference entre les solution de l'heuristique est celles de l'heuristique_statique

--- src/main.jl
+++ src/main.jl
@@ -13,25 +13,25 @@ n, s, t, S, d1, d2, p, ph, d, D = read_instance(file_name)
 #definition d'une durée maximum d'execution (en secondes)
 temps_max = 20.0
 
-# # Problème statique
-# isOptimal, x, obj, resolutionTime = pb_statique(n, s, t, S, p, d)
-# save_solution(isOptimal, x, obj, resolutionTime,  "statique", instance, s)
+# Problème statique
+isOptimal, x, obj, resolutionTime = pb_statique(n, s, t, S, p, d)
+save_solution(isOptimal, x, obj, resolutionTime,  "statique", instance, s)
 
-# # Resolution par dualisation
-# isOptimal, x, obj, resolutionTime = dualisation(n, s, t, S, d1, d2, p, ph, d, D, temps_max)
-# save_solution(isOptimal, x, obj, resolutionTime, "dualisation", instance, s)
+# Resolution par dualisation
+isOptimal, x, obj, resolutionTime = dualisation(n, s, t, S, d1, d2, p, ph, d, D, temps_max)
+save_solution(isOptimal, x, obj, resolutionTime, "dualisation", instance, s)
 
-# # Resolution par plan_coupants
-# solved, x, obj, resolutionTime = plan_coupants(n, s, t, S, d1, d2, p, ph, d, D, temps_max)
-# save_solution(solved, x, obj, resolutionTime, "plans_coupants", instance, s)
+# Resolution par plan_coupants
+solved, x, obj, resolutionTime = plan_coupants(n, s, t, S, d1, d2, p, ph, d, D, temps_max)
+save_solution(solved, x, obj, resolutionTime, "plans_coupants", instance, s)
 
-# # Résolution par branch-and-cut
-# solved, x, obj, resolutionTime = branch_and_cut(n, s, t, S, d1, d2, p, ph, d, D, temps_max)
-# save_solution(solved, x, obj, resolutionTime, "branch_and_cut", instance, s)
+# Résolution par branch-and-cut
+solved, x, obj, resolutionTime = branch_and_cut(n, s, t, S, d1, d2, p, ph, d, D, temps_max)
+save_solution(solved, x, obj, resolutionTime, "branch_and_cut", instance, s)
 
-# # Résolution du problème statique par heuristique
-# solved, path, obj, resolutionTime = heuristique_statique(n, s, t, S, p, d)
-# save_solution_heuristique(solved, path, obj, resolutionTime, instance)
+# Résolution du problème statique par heuristique
+solved, path, obj, resolutionTime = heuristique_statique(n, s, t, S, p, d)
+save_solution_heuristique_statique(solved, path, obj, resolutionTime, instance)
 
 # Résolution du problème robuste par heuristique
 solved, path, obj, resolutionTime = heuristique(n, s, t, S, d1, d2, p, ph, d, D)
